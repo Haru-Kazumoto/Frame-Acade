bfc675c34ba5209c267389af3748039dd6225116
Ikhsan892 <mfatihul8902@gmail.com>
2023-08-21 01:56:14 +0700

    fix : bug on recent courses

--- resources/views/my-course.blade.php
+++ resources/views/my-course.blade.php
@@ -30,9 +30,9 @@
                                     </div>
                                 </div>
                                 <a
-                                    href="{{ url("/learn/".$course->id) }}"
+                                    href="{{ url("/learn/".$recent->id) }}"
                                     class="inline-block rounded bg-blue-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-                                    Enrolled Now
+                                    Continue
                                 </a>
                             </div>
                         </div>
