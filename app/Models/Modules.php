81123bd8bcaddd7ae11db29ebaa7de3ed1205ab4
Haru-Kazumoto <jiaathaya@gmail.com>
2023-08-24 23:21:44 +0700

    feat : fix bug

--- app/Models/Modules.php
+++ app/Models/Modules.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\SubModules;
 
 class Modules extends Model {
     use HasFactory;
@@ -22,6 +22,6 @@ class Modules extends Model {
 
     //Modules ini berelasi one to many dari Submodules
     public function subModules(): HasMany{
-        return $this->hasMany(Submodules::class,"module_id","id");
+        return $this->hasMany(SubModules::class,"module_id","id");
     }
 }
