5c2fb5e18a2fab589602fdfb0bb329a0f32975b9
abay bay <akbar25005@gmail.com>
2023-08-24 08:58:35 +0700

    feat: completed certification

--- resources/views/certificate/certificate-content.blade.php
+++ resources/views/certificate/certificate-content.blade.php
@@ -1,157 +1,141 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Backend Developer Certificate</title>
     <style>
-        @import url("https://fonts.googleapis.com/css2?family=Imperial+Script&family=Poppins:wght@400;600&display=swap");
+        @import url("https://fonts.googleapis.com/css2?family=Imperial+Script&family=Playfair+Display:wght@400;500;600;700&family=Poppins:wght@400;600&display=swap");
 
-        * {
+        body {
             margin: 0;
             padding: 0;
-            box-sizing: border-box;
-        }
-
-        .container_certificate {
-            height: 100vh;
-            overflow: hidden;
-            z-index: 10;
             position: relative;
-            border: 1px solid black;
-            padding: 2rem;
-            display: flex;
         }
 
         .certificate {
-            z-index: 10;
-            width: 100%;
-            border: 8px solid black;
+            padding: 30px 30px;
+            text-align: center;
+            height: 622px;
             display: flex;
+            position: relative;
             flex-direction: column;
-            align-items: center;
             justify-content: center;
-            gap: 1rem;
+            border: 10px solid black;
         }
 
-        .certificate p {
+        .group {
+            margin-top: 8rem;
+        }
+
+        h1,
+        h2,
+        h3 {
+            margin: 5px 0;
+        }
+
+        h1 {
+            font-size: 60px;
+            font-family: "Playfair Display", serif;
+            letter-spacing: 10px;
+        }
+
+        h2 {
             font-family: "Poppins", sans-serif;
+            font-size: 28px;
         }
 
-        .certificate h1 {
-            font-family: "Imperial Script", cursive;
-            font-size: 90px;
-            font-weight: normal;
-        }
-        .certificate .name {
-            font-family: "Poppins", sans-serif;
-            font-size: 34;
-            font-weight: bolder;
-        }
-
-        .type_certification {
+        h3 {
             font-family: "Poppins", sans-serif;
             font-size: 38px;
         }
 
-        .logo {
-            display: flex;
-            align-items: center;
-            gap: 1rem;
-            margin-bottom: 1;
-        }
-
-        .logo h1 {
+        p {
             font-family: "Poppins", sans-serif;
-            font-size: 24px;
-            font-weight: bold;
+            margin: 15px 0;
         }
 
-        .logo img {
-            width: 140px;
+        .footer {
+            position: relative;
+            margin: 0 auto;
+            width: 40rem;
+        }
+
+        .footer-content {
+            flex: 1;
+            display: flex;
+            position: relative;
         }
 
         .divider {
-            width: 5px;
-            height: 65%;
             background-color: black;
+            height: 3px;
+            width: 200px;
         }
 
-        .left-top {
-            top: 0;
-            left: 0;
-            position: absolute;
-            background-color: #697899;
-            width: 300px;
-            height: 300px;
-            transform: rotate(45deg);
-            margin-top: -190px;
-            margin-left: -190px;
+        .founder {
+            margin: 5px 0;
         }
 
-        .right-top {
-            top: 0;
+        .date {
             right: 0;
             position: absolute;
-            background-color: #697899;
-            width: 300px;
-            height: 300px;
-            transform: rotate(45deg);
-            margin-top: -190px;
-            margin-right: -190px;
+            top: 50px;
         }
 
-        .left-bottom {
-            bottom: 0;
-            left: 0;
+        .logo {
             position: absolute;
-            background-color: #697899;
-            width: 300px;
-            height: 300px;
-            transform: rotate(-50deg);
-            margin-bottom: -170px;
-            margin-left: -190px;
+            top: 20px;
+            left: 20px;
         }
 
-        .right-bottom {
-            bottom: 0;
-            right: 0;
-            position: absolute;
-            background-color: #697899;
-            width: 300px;
-            height: 300px;
-            transform: rotate(50deg);
-            margin-bottom: -170px;
-            margin-right: -190px;
+        .logo_image {
+            width: 170px;
         }
 
+        .logo-signature {
+            position: relative;
+            width: 200px;
+        }
 
+        .signature {
+            width: 100px;
+        }
+
+        @media print {
+            .certificate {
+                background-color: white;
+            }
+        }
     </style>
 </head>
+
 <body>
-<div class="container_certificate">
     <div class="certificate">
-        <div class="left-top"></div>
-        <div class="left-bottom"></div>
-        <div class="right-top"></div>
-        <div class="right-bottom"></div>
-        <h1>Certification</h1>
-        <p>this certifies that</p>
-        <h2 class="name">Ziaurrahman Athaya</h2>
-        <p>has successfully completed</p>
-        <h2 class="type_certification">Backend Developer Course</h2>
-        <p class="date">On {{ now() }} At</p>
         <div class="logo">
-            <img src="./Logo-Dark.png" alt="logo" />
-            <div class="divider"></div>
-            <div class="frameacade">
-                <h1>Frame</h1>
-                <h1 style="color: #f6b21b; margin-left: 1.5rem">
-                    Acade
-                </h1>
+            <img src="./Logo-Dark.png" alt="" class="logo_image" />
+        </div>
+        <div class="group">
+            <h1 class="certification">CERTIFICATE</h1>
+            <p>This is to certify that</p>
+            <h2>Ziaurrahman Athaya</h2>
+            <p>has successfully completed the</p>
+            <h3>Backend Developer Course</h3>
+            <p>
+                with a deep understanding of backend technologies and
+                development.
+            </p>
+            <div class="footer">
+                <div class="logo-signature">
+                    <img src="./signature.png" alt="" class="signature" />
+                    <div class="divider"></div>
+                    <p class="founder">CEO FrameAcade</p>
+                </div>
+                <p class="date">August 23, 2023</p>
             </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
